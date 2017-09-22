53e2a2ea0698949670b1ad5eb02a76f570db3810
lightpine <lightpine.natural@gmail.com>
2017-09-22 17:15:13 +0900

    2017_09_22_Fir

---
+++ java-basic/examples/Test06_other.java
@@ -0,0 +1,9 @@
+class Test06_other
+
+{
+	public static void printHello(){
+
+		System.out.println("Hello!");
+			
+	}
+}
---
+++ java-basic/src/main/java/Test09_3.java
@@ -0,0 +1,12 @@
+/*  클래스와 패키지
+  - 패키지를 사용하여 클래스를 분류하는 방법을 알아보자
+*/
+package a.b.c; // 하위 패키지는 .으로 구분한다.
+
+public class Test09_3{
+
+      public static void main(String[] args){
+        System.out.println("Hello");
+      }
+}
+

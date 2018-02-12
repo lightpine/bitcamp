0ae1a174618f77c9cd77a4553a6486803e7f004d
lightpine <lightpine.natural@gmail.com>
2018-02-12 10:10:13 +0900

    'test'

--- java-project/src/main/java/java100/app/Test.java
+++ java-project/src/main/java/java100/app/Test.java
@@ -1,7 +1,5 @@
 package java100.app;
 
-import org.springframework.context.annotation.Bean;
-
 public class Test {
 
     public static void main(String[] args) {
--- java-project/src/main/java/java100/app/web/json/ScoreController.java
+++ java-project/src/main/java/java100/app/web/json/ScoreController.java
@@ -3,7 +3,6 @@ package java100.app.web.json;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
